dd855af2a7de046b98e5e17554f73fe1cbe2a354
mmast38 <mastafa2618@gmail.com>
2025-04-02 19:50:46 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Batch38!");
+        System.out.println("Welcome to the DevOps 55-Day Training Course!!!");
     }
 
    
