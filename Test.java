41d69d07cbcf7de0f62172d537794bdf58c0d10c
mmast38 <mastafa2618@gmail.com>
2025-04-02 19:58:08 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the DevOps 55-Day Training Course!!!");
+        System.out.println("Welcome to java!!!");
     }
 
    
